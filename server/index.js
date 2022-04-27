4974190b8429a4c1ed5aee50a5c1b6eaea122f0e
Spencer DeBrine <debst12@gmail.com>
2022-04-27 14:48:23 -0600

    added css end point

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'../index.html'))
 })
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname,'../styles.css'))
+})
 
 const port = process.env.PORT || 4005
 
